eeea6455f2845fe6503f6f502bff79156d45a306
Muhammad Subhan Khan <subbikhan124@gmail.com>
2025-01-23 01:41:24 +0500

    deleted files

--- backend/api/final.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-
-app.listen(2000,()=>{
-    console.log("Server has started");
-})
--- backend/api/index.js
+++ backend/api/index.js
@@ -7,23 +7,17 @@ const NGORoutes = require('./Routers/NGORouters');
 const userRouter = require('./api/user/user.router');
 
 let app = express();
-app.options('*', cors()); // Allow preflight requests
+// Middleware to parse JSON
+app.use(express.json());
 
-// Middleware to enable CORS
-app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', 'https://learn-nmuga4b6l-erajts-projects.vercel.app'); // Adjust the origin as needed
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-  next();
-});
+// Enable CORS only for your frontend origin
+const corsOptions = {
+  origin: ['http://localhost:3000', 'rewear-hub.vercel.app'],
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-app.use(cors({
-  // origin: 'https://learn-nmuga4b6l-erajts-projects.vercel.app', // Allow requests from frontend application
-  origin: 'http://localhost:5173',
-  credentials: true // Allow credentials (cookies) to be included with requests
-}));
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json({ limit: '200mb' }))
 app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
