0d1edb2db95a3e39ffdb5ab5e5822bda322753b2
Eraj Tanweer <erajtanweer2@gmail.com>
2025-01-22 13:09:57 +0500

    abcd

---
+++ backend/config/databsase.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+//Database Connection
+
+const connection=mysql.createPool({
+    connectionLimit: 6,
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "learn",
+    port: 3306,
+    ssl: false
+    // ssl: false
+    
+})
+connection.getConnection((err,connection)=>{
+    if (err){
+        return console.log(err);
+    }
+    connection.release();
+    console.log("Database connected successfully!");
+})
+
+module.exports = connection;
